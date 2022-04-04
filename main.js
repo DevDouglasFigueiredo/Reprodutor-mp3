604fb5b62ff1bbded2888183e96889deccbeae1b
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-04-04 18:09:51 -0300

    update file

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 let musica = document.querySelector('audio')
+
+document.querySelector('.icon-play').addEventListener('click', tocarMusica);
+document.querySelector('.icon-pause').addEventListener('click', pausarMusica);
+
+function tocarMusica(){
+    musica.play();
+    document.querySelector('.icon-pause').style.display = 'block';
+    document.querySelector('.icon-play').style.display = 'none';
+}
+
+function pausarMusica(){
+    musica.pause();
+    document.querySelector('.icon-pause').style.display = 'none';
+    document.querySelector('.icon-play').style.display = 'block';
+}
