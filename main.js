62fa2b7375e9fb9a6eba1bd1ca27b3cdde7bce00
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-04-07 00:48:49 -0300

    exercising

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var music = document.querySelector('audio');
-var barra = document.querySelector('progress');
 var tempoDecorrido = document.querySelector('.inicio');
+var barra = document.querySelector('progress');
 
+document.querySelector('.icon-play').addEventListener('click', playMusic);
+document.querySelector('.icon-pause').addEventListener('click', pauseMusic);
+music.addEventListener('timeupdate', movimentaBarra)
 
-document.querySelector('.icon-play').addEventListener('click' , playMusic);
-document.querySelector('.icon-pause').addEventListener('click' , pauseMusic);
-music.addEventListener('timeupdate', movimentaBarra);
 
 function playMusic(){
     music.play();
@@ -13,25 +13,23 @@ function playMusic(){
     document.querySelector('.icon-pause').style.display = 'block';
 }
 
-function pauseMusic(){                                                      // 10 min do video
+function pauseMusic(){
     music.pause();
     document.querySelector('.icon-play').style.display = 'block';
     document.querySelector('.icon-pause').style.display = 'none';
 }
 
-function movimentaBarra(){                                                  // 19:30 do video
-    barra.style.width = Math.floor((music.currentTime / music.duration) * 100) + '%';
+function movimentaBarra(){
+    barra.style.width = Math.floor((music.currentTime / music.duration) * 100) + '%' ;
     tempoDecorrido.textContent = segundosParaMinutos(Math.floor(music.currentTime));
 }
 
-function segundosParaMinutos(segundos){                                     // 28:00 do video
-    var campoMinutos = Math.floor(segundos / 60);
+function segundosParaMinutos (segundos){
+    var campoMinutos = Math.floor(segundos / 60) ;
     var campoSegundos = segundos % 60;
-    if(campoSegundos < 10){
-        campoSegundos = "0" + campoSegundos
+    if( campoSegundos < 10){
+        campoSegundos = '0' + campoSegundos;
     }
-    return campoMinutos + ":" + campoSegundos;
-    
+    return campoMinutos + ":" + campoSegundos
 
 }
-
